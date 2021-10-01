df2c4a261045fcee3e5f14a4451eb5e91007a929
cliftondavies <clifton.davies@outlook.com>
2021-10-01 23:27:26 +0100

    Create home page

---
+++ src/pages/home/home.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Directory from '../../components/directory/directory.component';
+import './home.styles.scss';
+
+const Home = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
+
+export default Home;
