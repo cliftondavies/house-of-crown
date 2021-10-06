2d349f2238c9ebe1df4b6ae0d674d38263878837
cliftondavies <clifton.davies@outlook.com>
2021-10-06 20:53:53 +0100

    Define actions, reducer, and selectors for the cart state

---
+++ src/redux/cart/cart.reducer.js
@@ -0,0 +1,36 @@
+import cartActionTypes from './cart.types';
+import { addItemToCart, clearItemFromCart, removeItemFromCart } from './cart.utils';
+
+const INITIAL_STATE = {
+  hidden: true,
+  cartItems: []
+};
+
+const cartReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case cartActionTypes.TOGGLE_CART_HIDDEN:
+      return {
+        ...state,
+        hidden: !state.hidden
+      };
+    case cartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload)
+      };
+    case cartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: clearItemFromCart(state.cartItems, action.payload)
+      };
+    case cartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
+      };
+    default:
+      return state;
+  }
+};
+
+export default cartReducer;
