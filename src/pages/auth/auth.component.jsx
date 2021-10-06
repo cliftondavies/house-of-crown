f99a1ac09b2ddf125b87f1812360d7d5beeee4cc
cliftondavies <clifton.davies@outlook.com>
2021-10-06 21:08:26 +0100

    Implement the authentication page

---
+++ src/pages/auth/auth.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
+
+import './auth.styles.scss';
+
+const Auth = () => (
+  <div className='auth'>
+    <SignIn />
+    <SignUp />
+  </div>
+);
+
+export default Auth;
