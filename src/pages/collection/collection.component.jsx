dc6893065b97c4517756913a20d00697f5cbc3e6
cliftondavies <clifton.davies@outlook.com>
2021-10-21 17:32:02 +0100

    Normalised shop data and streamline redux data flow

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -7,13 +7,21 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ collection }) => (
-  <div className='collection-page'>
-    {collection.items.map(item => (
-      <CollectionItem key={item.id} item={item} />
-    ))}
-  </div>
-);
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
+
+  return (
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+
+      <div className='items'>
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  )
+};
 
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state)
